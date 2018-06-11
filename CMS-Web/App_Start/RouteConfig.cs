87fca06acc2bea5f39810350e6ceab4dbf9edcd5
DESKTOP-VB32SNB\Kenshin <tunx406.ktl@gmail.com>
2018-06-11 20:36:13 +0700

    [M] Update source

--- CMS-Web/App_Start/RouteConfig.cs
+++ CMS-Web/App_Start/RouteConfig.cs
@@ -15,7 +15,7 @@ namespace CMS_Web
             routes.MapRoute(
                  "Default", // Route name
                  "{controller}/{action}/{id}", // URL with parameters
-                 new { area = "Admin", controller = "CMSEmployees", action = "Index", id = UrlParameter.Optional }, // Parameter defaults
+                 new { area = "Admin", controller = "Home", action = "Index", id = UrlParameter.Optional }, // Parameter defaults
                  null,
                  new[] { "CMS_Web.Areas.Admin.Controllers" }
              ).DataTokens.Add("area", "Admin");
