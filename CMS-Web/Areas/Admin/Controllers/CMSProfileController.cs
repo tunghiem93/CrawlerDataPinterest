1093cab9ed2f5bafd22593a4c9b9454345291251
Quang Tran <quangtrantpq@gmail.com>
2018-06-13 13:21:46 +0700

    [M] update controller CMSProfile.

--- CMS-Web/Areas/Admin/Controllers/CMSProfileController.cs
+++ CMS-Web/Areas/Admin/Controllers/CMSProfileController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CMS_Web.Web.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace CMS_Web.Areas.Admin.Controllers
     public class CMSProfileController : Controller
     {
         // GET: Admin/CMSProfile
+        [NuAuth]
         public ActionResult Index()
         {
             return View();
