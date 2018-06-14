0977ed3fa545f6a4eadabb54b9f41a40aa4f2227
Quang Tran <quangtrantpq@gmail.com>
2018-06-14 17:40:13 +0700

    [M] Pin Factory: add func Get Pin

--- CMS-Shared/CMSPins/CMSPinFactory.cs
+++ CMS-Shared/CMSPins/CMSPinFactory.cs
@@ -2,6 +2,7 @@
 using CMS_DTO.CMSEmployee;
 using CMS_Entity;
 using CMS_Entity.Entity;
+using CMS_Shared.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,7 +29,7 @@ namespace CMS_Shared.CMSEmployees
                     var lstPinUpdate = _db.CMS_Pin.Where(o => lstPinID.Contains(o.ID)).ToList();
                     var lstPinUpdateID = lstPinUpdate.Select(o => o.ID).ToList();
                     var lstPinInsert = lstPin.Where(o => !lstPinUpdateID.Contains(o.ID)).ToList();
-                    
+
                     /* update pin */
                     foreach (var uPin in lstPinUpdate)
                     {
@@ -46,7 +47,7 @@ namespace CMS_Shared.CMSEmployees
                             ID = pin.ID,
                             Link = pin.Link,
                             Repin_count = pin.Repin_count,
-                            ImageUrl = pin.Images.Select(o=> o.url).First(),
+                            ImageUrl = pin.Images.Select(o => o.url).First(),
                             Created_At = pin.Created_At,
                             Status = (byte)Commons.EStatus.Active,
                             CreatedBy = createdBy,
@@ -90,6 +91,51 @@ namespace CMS_Shared.CMSEmployees
             finally
             {
                 m_Semaphore.Release();
+            }
+            return result;
+        }
+
+        public bool GetPin(ref List<PinsModels> lstPins, string KeyWordID, ref string msg)
+        {
+            var result = true;
+            lstPins = new List<PinsModels>();
+
+            try
+            {
+                using (var _db = new CMS_Context())
+                {
+                    if (!string.IsNullOrEmpty(KeyWordID))
+                    {
+                        lstPins = _db.CMS_R_KeyWord_Pin.Where(o => o.KeyWordID == KeyWordID)
+                            .Join(_db.CMS_Pin, kp => kp.PinID, p => p.ID, (kp, p) => p)
+                            .Select(o => new PinsModels()
+                            {
+                                ID = o.ID,
+                                Link = o.Link,
+                                Domain = o.Domain,
+                                Repin_count = o.Repin_count ?? 0,
+                                Images = new List<ImageModels>()
+                                {
+                                    new ImageModels()
+                                    {
+                                        url = o.ImageUrl,
+                                    },
+                                },
+                                Created_At = o.Created_At ?? DateTime.MinValue,
+                                CreatedDate = o.CreatedDate ?? DateTime.MinValue,
+                                UpdateDate = o.UpdatedDate ?? DateTime.MinValue,
+                                LastTime = CommonHelper.GetDurationFromNow(o.UpdatedDate),
+                            }).ToList();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = "CreateOrUpdate Pin with exception.";
+                result = false;
+            }
+            finally
+            {
             }
             return result;
         }
