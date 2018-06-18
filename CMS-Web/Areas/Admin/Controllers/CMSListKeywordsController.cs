562d491dca7e6162939754dead6bba1a09dde2f5
Kenshin_Tu <tunx406.ktl@gmail.com>
2018-06-18 09:33:36 +0700

    [+] add module ListKeyword

---
+++ CMS-Web/Areas/Admin/Controllers/CMSListKeywordsController.cs
@@ -0,0 +1,35 @@
+﻿using CMS_DTO.CMSKeyword;
+using CMS_Shared.CMSListKeyword;
+using CMS_Web.Web.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS_Web.Areas.Admin.Controllers
+{
+    [NuAuth]
+    public class CMSListKeywordsController : Controller
+    {
+        private CMSListKeywordFactory _factory;
+
+        public CMSListKeywordsController()
+        {
+            _factory = new CMSListKeywordFactory();
+        }
+
+        // GET: Admin/CMSListKeywords
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoadGrid()
+        {
+            var model = new CMS_KeywordModels(); // _factory.GetList();
+            return PartialView("_ListData", model);
+        }
+
+    }
+}
