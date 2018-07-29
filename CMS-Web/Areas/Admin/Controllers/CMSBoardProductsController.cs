183df6bdf0fbe5ed9aec9dab4867d408d8784c12
Kenshin_Tu <tunx406.ktl@gmail.com>
2018-07-29 23:29:48 +0700

    [+] and [M] Update source

--- CMS-Web/Areas/Admin/Controllers/CMSBoardProductsController.cs
+++ CMS-Web/Areas/Admin/Controllers/CMSBoardProductsController.cs
@@ -1,17 +1,229 @@
-﻿using System;
+﻿using CMS_DTO.CMSCrawler;
+using CMS_DTO.CMSProduct;
+using CMS_Shared;
+using CMS_Shared.CMSBoard;
+using CMS_Shared.CMSEmployees;
+using CMS_Shared.Utilities;
+using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace CMS_Web.Areas.Admin.Controllers
 {
-    public class CMSBoardProductsController : Controller
+    public class CMSBoardProductsController : BaseController
     {
-        // GET: Admin/CMSBoardProducts
+        private CMSPinFactory _fac;
+        private CMSBoardFactory _facBoard;
+        public CMSBoardProductsController()
+        {
+            _fac = new CMSPinFactory();
+            _facBoard = new CMSBoardFactory();
+        }
+        // GET: Admin/CMSCategories
         public ActionResult Index()
         {
-            return View();
+            CMS_ProductsModels model = new CMS_ProductsModels();
+            try
+            {
+                var _Key = Request["BoardID"] ?? "";
+                var _Group = Request["GroupID"] ?? "";
+                var FilterModel = new PinFilterDTO();
+                FilterModel.PageIndex = Commons.PageIndex;
+                FilterModel.PageSize = Commons.PageSize;
+                FilterModel.TypeTime = Commons.ETimeType.TimeReduce.ToString("d");
+                model.ListTime = getListTime();
+                model.ListQuantity = getListQuantity();
+                // model.ListRePin = getListRepinCount();
+                ViewBag.Keywords = getListKeyword();
+                if (!string.IsNullOrEmpty(_Key))
+                {
+                    FilterModel.LstKeyWordID.Add(_Key);
+                    model.listKeywords.Add(_Key);
+                }
+                if (!string.IsNullOrEmpty(_Group))
+                {
+                    FilterModel.LstGroupID.Add(_Group);
+
+                    var _lstKeywords = getListKeyWordByGroup(_Group);
+                    model.listKeywords.AddRange(_lstKeywords);
+                }
+
+                var _pinModels = new List<PinsModels>();
+                var msg = "";
+                int totalPin = 0;
+                var result = _fac.GetPin(ref _pinModels, ref totalPin, FilterModel, ref msg);
+                if (result)
+                {
+                    model.Crawler.Pins = _pinModels.OrderBy(x => x.Created_At).ToList();
+                    #region "Comment"
+                    //if (TypeTime.Equals(Commons.ETimeType.TimeReduce.ToString("d")))
+                    //{
+                    //    model.Crawler.Pins = model.Crawler.Pins.OrderByDescending(x => x.Created_At).ToList();
+                    //}
+                    //else if (TypeTime.Equals(Commons.ETimeType.TimeIncrease.ToString("d")))
+                    //{
+                    //    model.Crawler.Pins = model.Crawler.Pins.OrderBy(x => x.Created_At).ToList();
+                    //}
+                    //else if (TypePin.Equals(Commons.ETimeType.PinReduce.ToString("d")))
+                    //{
+                    //    model.Crawler.Pins = model.Crawler.Pins.OrderByDescending(x => x.Repin_count).ToList();
+                    //}
+                    //else if (TypePin.Equals(Commons.ETimeType.PinIncrease.ToString("d")))
+                    //{
+                    //    model.Crawler.Pins = model.Crawler.Pins.OrderBy(x => x.Repin_count).ToList();
+                    //}
+                    #endregion
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+            return View(model);
+        }
+
+        public ActionResult LoadScroll(PinFilterDTO pinFilter)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(pinFilter.Url))
+                {
+                    NameValueCollection QueryString = CommonHelper.GetQueryParameters(pinFilter.Url);
+                    var _Key = QueryString["keywordID"] ?? "";
+                    var _Group = QueryString["GroupID"] ?? "";
+
+                    if (!string.IsNullOrEmpty(_Key))
+                    {
+                        pinFilter.LstKeyWordID.Add(_Key);
+                    }
+                    if (!string.IsNullOrEmpty(_Group))
+                    {
+                        pinFilter.LstGroupID.Add(_Group);
+
+                        var _lstKeywords = getListKeyWordByGroup(_Group);
+                        pinFilter.LstKeyWordID.AddRange(_lstKeywords);
+                    }
+                }
+
+                if (pinFilter.LstKeyWordID != null && pinFilter.LstKeyWordID.Count > 0)
+                {
+                    if (string.IsNullOrEmpty(pinFilter.LstKeyWordID[0]))
+                        pinFilter.LstKeyWordID = null;
+                }
+                var modelCrawler = new CMS_CrawlerModels();
+                var _pinModels = new List<PinsModels>();
+                var msg = "";
+                pinFilter.PageSize = Commons.PageSize;
+                int totalPin = 0;
+                var result = _fac.GetPin(ref _pinModels, ref totalPin, pinFilter, ref msg);
+                if (result)
+                {
+                    modelCrawler.Pins = _pinModels;
+                }
+                return PartialView("_ListItem", modelCrawler);
+            }
+            catch (Exception) { }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        public ActionResult Search()
+        {
+            try
+            {
+                var FilterModel = new PinFilterDTO();
+                FilterModel.PageIndex = Commons.PageIndex;
+                FilterModel.PageSize = Commons.PageSize;
+                FilterModel.CreatedDateFrom = null;
+                FilterModel.CreatedDateTo = null;
+                // var _Key = Request["Key"] ?? "";
+                var TypeTime = Request["TypeTime"] ?? "";
+                //  var TypePin = Request["TypePin"] ?? "";
+                var _TypeQuantity = Request["TypeQuantity"];
+                int TypeQuantity = -1;
+                if (!string.IsNullOrEmpty(_TypeQuantity))
+                {
+                    TypeQuantity = Convert.ToInt16(_TypeQuantity);
+                }
+                var Keywords = Request["listKeywords"] ?? null;
+                char[] separator = new char[] { ',' };
+                var ListKeyword = CommonHelper.ParseStringToList(Keywords, separator);
+                var _FromDate = Convert.ToDateTime(Request["FromDate"]);
+                var _ToDate = Convert.ToDateTime(Request["ToDate"]);
+                #region "comment"
+                //cache data
+                //Response.Cookies["TypeTime"].Value = TypeTime.ToString();
+                //Response.Cookies["TypeTime"].Expires = DateTime.Now.AddYears(1); // add expiry time
+
+                //Response.Cookies["TypePin"].Value = TypePin.ToString();
+                //Response.Cookies["TypePin"].Expires = DateTime.Now.AddYears(1); // add expiry time
+
+                //Response.Cookies["FromDate"].Value = _FromDate.ToString();
+                //Response.Cookies["FromDate"].Expires = DateTime.Now.AddYears(1); // add expiry time
+                //Response.Cookies["ToDate"].Value = _ToDate.ToString();
+                //Response.Cookies["ToDate"].Expires = DateTime.Now.AddYears(1); // add expiry time
+                //if(TypeQuantity != 0)
+                //{
+                //    Response.Cookies["TypeQuantity"].Value = TypeQuantity.ToString();
+                //    Response.Cookies["TypeQuantity"].Expires = DateTime.Now.AddYears(1); // add expiry time
+                //}
+                #endregion
+                FilterModel.CreatedAtFrom = _FromDate;
+                FilterModel.CreatedAtTo = _ToDate;
+                if (TypeQuantity.ToString() == Commons.EQuantityType.ZeroToOne.ToString("d"))
+                {
+                    FilterModel.PinCountFrom = 0;
+                    FilterModel.PinCountTo = 100;
+                }
+                if (TypeQuantity.ToString() == Commons.EQuantityType.OneToTwo.ToString("d"))
+                {
+                    FilterModel.PinCountFrom = 100;
+                    FilterModel.PinCountTo = 200;
+                }
+                if (TypeQuantity.ToString() == Commons.EQuantityType.TwoToThree.ToString("d"))
+                {
+                    FilterModel.PinCountFrom = 200;
+                    FilterModel.PinCountTo = 300;
+                }
+                if (TypeQuantity.ToString() == Commons.EQuantityType.ThreeToFour.ToString("d"))
+                {
+                    FilterModel.PinCountFrom = 300;
+                    FilterModel.PinCountTo = 400;
+                }
+                if (TypeQuantity.ToString() == Commons.EQuantityType.FourToFive.ToString("d"))
+                {
+                    FilterModel.PinCountFrom = 400;
+                    FilterModel.PinCountTo = 500;
+                }
+                if (TypeQuantity.ToString() == Commons.EQuantityType.MoreFive.ToString("d"))
+                {
+                    FilterModel.PinCountFrom = 500;
+                }
+
+                if (ListKeyword != null && ListKeyword.Count > 0)
+                {
+                    FilterModel.LstKeyWordID = ListKeyword;
+                    // Response.Cookies["Keywords"].Value = Keywords.ToString();
+                    //  Response.Cookies["Keywords"].Expires = DateTime.Now.AddYears(1); // add expiry time
+                }
+                FilterModel.TypeTime = TypeTime;
+
+                var modelCrawler = new CMS_CrawlerModels();
+                var _pinModels = new List<PinsModels>();
+                var msg = "";
+                int totalPin = 0;
+                var result = _fac.GetPin(ref _pinModels, ref totalPin, FilterModel, ref msg);
+                if (result)
+                {
+                    modelCrawler.Pins = _pinModels;
+
+                }
+                return PartialView("_ListItem", modelCrawler);
+            }
+            catch (Exception ex) { }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
         }
     }
 }
---
+++ CMS-Web/Areas/Admin/Controllers/CMSBoardsController.cs
@@ -0,0 +1,198 @@
+﻿using CMS_DTO.CMSBoard;
+using CMS_DTO.CMSCrawler;
+using CMS_Shared.CMSAccount;
+using CMS_Shared.CMSBoard;
+using CMS_Shared.Utilities;
+using CMS_Web.Web.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMS_Web.Areas.Admin.Controllers
+{
+    [NuAuth]
+    public class CMSBoardsController : BaseController
+    {
+        // GET: Admin/GroupSearchs
+        private CMSBoardFactory _factory;
+        private CMSAccountFactory _facAcc;
+        private List<string> ListItem = null;
+        public CMSBoardsController()
+        {
+            _factory = new CMSBoardFactory();
+            _facAcc = new CMSAccountFactory();
+            ListItem = new List<string>();
+            ListItem = _factory.GetList().Select(o => o.name).ToList();
+            ViewBag.ListGroupKey = getListGroupKeyword();
+        }
+        // GET: Admin/CMSBoard
+        public ActionResult Index()
+        {
+            List<CMS_BoardModels> models = new List<CMS_BoardModels>();
+            List<SelectListItem> lstAcc = new List<SelectListItem>();
+            var lstAccount = _facAcc.GetList();
+            if (lstAccount != null && lstAccount.Any())
+            {
+                lstAccount.ForEach(o =>
+                {
+                    lstAcc.Add(new SelectListItem
+                    {
+                        Value = o.Id,
+                        Text = o.Account
+                    });
+                });
+            }
+            ViewBag.ListAccount = lstAcc;
+            try
+            {
+                CrawlerBoardHelper._Cookies = "G_ENABLED_IDPS=google; _b=\"ATW1MZ0GSMJI46hOmoQ3MpWpXe6+rP8fkEoLnaSC6XQ7dd54Cm+VLWXBJOy2qLRza8E=\"; _auth=1; csrftoken=g9vz7YbodrSvIMzOaDUOcDLGNhdKJP1v; _pinterest_sess=\"TWc9PSZsVVROcGxoeVNLOE1TK2h6UVlDcU1JS0JqMkxIb2M3Vi9OMzYvMEZydnEzOGdKYzh3M0tsN3hKRndUeVBzK1RmQXhsUmNVTkRpN0h4S21HTHRpeWRHd2ZSVGlHMWhoenBVZTFDYWdZdFBKK3lNOFlrSkt3MmVmOE1qUGJ6cmFtbitqQzZTSDJKenRhM0JpTmhGU1pRaFZ6NXc0MlVkcTlZdDJIYnlsenNvMlYrMmlkam5VVEVPTnhEQmxHcUxWNTFUTWc4amxQVnpBOEtWM29TVTV1K0JWK3pjNFpseXVTNGhTNTN4NUQ4QTRvdUYwdWo4ZzJDdFpjRE1PazdTVFJwcVM2UnVGVzl5U2lXZldic2xlUVdMdFBqK3hTTXJqazl2R21ibjYyMFhUYkYxR2xRMjk4ZG9ZKzI4ck85TGVaUUNVMzVDZUk3R285eHk0bnEyVmEwSkNLMmp2ZjlGSWtGbzJ6QXV6Y0NnU0FHZm9vQ292RklwRE5tSGZtSkRPTHEreU81VnkvbkZoUXA5SXRkRmNLNXkxUktPNXhFVDlBNHpJSkZ3T1dmSUdqaElJbmFOS2pIcVpFam44WjlicEU2T3NqbnRGUkx3UkVsRzA5RDI3SGI4NFVrK3c4S2FjaU9UbCtvOHJDVU9JU2JjNndOK2lLWkVlQ1lpcHhDOXdIY2FSY1Z4NXNtR1dqV1g0aTA0WURzSEtOOWdHcFpld2t2ZGxscTh6RDY2SHpYVFFiM2JFU0FYNzZndkJ0OTliY2MyRHhTWGVsWDFHQkd6NXlZbnVzQkkyOHJxNEdyQWhoVDRJaXAzQTBhNGJMbUJ1RXRwUTNIZDlKZi9UcFBrQm5ZRlpsRlJFY3NuUnJFYUdEZEQvYUlOVXUyNGUvaURiQXQ1ZDZKZU9DMXJtV3NWakdyazdLSnZrMndiWE1uSmM5UncrN0hvTWwwN1g4azFUVUM2R21LUFN5Z1IvSVhxSmVPYm81QWo5R1VVRDdycUJxWnpsZWFvcVpCOHVpK3JnVmYwTHlNU0hRNEJwak1EYkVTMS8yRnd3VlBYNXlDNmI5Y2s2clFxa3hLcFJmODFjT0s3RXk0dEJuWTl4ZTdWU1BBJkF4Tm5NSWQ0UUo3RW9nSzJkUWVDaXJ1a1JOTT0=\"; _ga=GA1.2.729252838.1528255220; cm_sub=none; sessionFunnelEventLogged=1; bei=false";
+                CrawlerBoardHelper.Get_Tagged_Board(ref models, "Footballs", 2, null);
+            }
+            catch(Exception ex)
+            {
+                NSLog.Logger.Error("CMSBoard_Index", ex);
+            }
+            return View(models);
+        }
+
+        public ActionResult LoadGrid(CMS_BoardModels item)
+        {
+            try
+            {
+                var msg = "";
+                bool isCheck = true;
+                if (item.name != null && item.name.Length > 0)
+                {
+                    var temp = ListItem.Where(o => o.Trim() == item.name.Trim()).FirstOrDefault();
+                    if (temp == null)
+                    {
+                        var result = _factory.CreateOrUpdate(item, ref msg);
+                        if (!result)
+                        {
+                            isCheck = false;
+                        }
+                    }
+                    else
+                    {
+                        ViewBag.DuplicateKeyword = "Duplicate Board!";
+                    }
+                }
+                if (isCheck)
+                {
+                    var model = _factory.GetList();
+                    return PartialView("_ListData", model);
+                }
+                else
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            catch (Exception e)
+            {
+                //_logger.Error("Keyword_Search: " + e);
+                return new HttpStatusCodeResult(400, e.Message);
+            }
+        }
+
+        public ActionResult AddBoardToGroup(string KeyID, string GroupKeyID)
+        {
+            var msg = "";
+            var result = _factory.AddBoardToGroup(KeyID, GroupKeyID, ref msg);
+            if (result)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        public ActionResult RemoveBoardFromGroup(string KeyID, string GroupKeyID)
+        {
+            var msg = "";
+            var result = _factory.RemoveKeyFromGroup(KeyID, GroupKeyID, ref msg);
+            if (result)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        #region For row
+        public ActionResult CrawlerBoard(string ID, string Key)
+        {
+            var msg = "";
+            new Thread(() => { _factory.CrawlData(ID, "Admin", ref msg); }).Start();
+            var result = true;
+            if (result)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        public ActionResult Delete(string ID)
+        {
+            var msg = "";
+            var result = _factory.Delete(ID, "Admin", ref msg);
+            if (result)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        public ActionResult DeleteAndClearPost(string ID)
+        {
+            var msg = "";
+            //var result = _factory.DeleteAndRemoveDB(ID, ref msg);
+            var result = _factory.DeleteAndRemoveDBCommand(ID, ref msg);
+
+            if (result)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+        #endregion
+
+        #region For All
+        public ActionResult BoardCrawlAll(string ID, string Key)
+        {
+            var msg = "";
+            //_factory.CrawlAllKeyWords("Admin", ref msg);
+            new Thread(() => { _factory.CrawlAllKeyWords("Admin", ref msg); }).Start();
+
+            var result = true;
+            if (result)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        public ActionResult DeleteAll()
+        {
+            var msg = "";
+            var result = _factory.DeleteAll("Admin", ref msg);
+            if (result)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        public ActionResult DeleteAndClearPostAll()
+        {
+            var msg = "";
+            var result = _factory.DeleteAndRemoveDBAll(ref msg);
+
+            if (result)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.OK);
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+        #endregion
+    }
+}
